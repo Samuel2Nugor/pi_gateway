7937c44aedfe7fec45cd8d68de59403dbb1b1010
Samuel2Nugor <wizlek12@gmail.com>
2026-04-24 13:34:24 +0200

    Add MQTT payload validation

--- mqtt_client.py
+++ mqtt_client.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
-from config import MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_TOPIC_TO_TAG
+import json
 
+from config import MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_TOPIC_TO_TAG
 from ble_client import run_ble_write
 
 
@@ -12,6 +13,18 @@ def on_connect(client, userdata, flags, reason_code, properties=None):
 def on_message(client, userdata, message):
     payload = message.payload.decode("utf-8")
     print(f"Recieved on topic {message.topic}: {payload}")
+
+    try:
+        data = json.loads(payload)
+    except json.JSONDecodeError:
+        print("Invalid JSON. Message ignored.")
+
+    requires_fields = ["tagId", "title", "finalPrice"]
+
+    for field in requires_fields:
+        if field not in data:
+            print(f"Missing field: {field}. Message ignored")
+            return
 
     run_ble_write(payload)
 
